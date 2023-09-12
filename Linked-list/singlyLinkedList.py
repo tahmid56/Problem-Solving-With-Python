225fec054ed55a8a334480aae0edb76528dc23ff
Tahmid Akter <tahmidakter56@gmail.com>
2023-09-12 14:42:15 +0600

    palindrome array approach

--- Linked-list/singlyLinkedList.py
+++ Linked-list/singlyLinkedList.py
@@ -89,6 +89,23 @@ class SinglyLinkedList:
             curr = curr.next
         self.printList(dummy.next)
 
+    def palindrome(self, head):
+        #Array Approach - O(n) space 
+        nums =[]
+        while head:
+            nums.append(head.val)
+            head = head.next
+        l, r = 0, len(nums) - 1
+        while(l <= r):
+            if nums[l] != nums[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+        
+        # O(1) space approach
+
+
 ll = SinglyLinkedList();
 
 ll.append(5)
