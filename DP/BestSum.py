ed65d6baeffb60e2f74a7c64620a714138863e30
Tahmid Akter <tahmidakter56@gmail.com>
2023-10-03 02:24:55 +0600

    memoization adde to bestsum

--- DP/BestSum.py
+++ DP/BestSum.py
@@ -1,4 +1,6 @@
-def bestSum(targetSum, numbers):
+def bestSum(targetSum, numbers, memo={}):
+    if(targetSum in memo):
+        return memo[targetSum]
     if(targetSum == 0):
         return []
     if(targetSum < 0):
@@ -7,11 +9,12 @@ def bestSum(targetSum, numbers):
 
     for num in numbers:
         remainder = targetSum - num
-        remainderCombination = bestSum(remainder, numbers)
+        remainderCombination = bestSum(remainder, numbers, memo)
         if(remainderCombination != None):
             combination = [*remainderCombination, num]
             if(shortestCombination == None or len(combination) < len(shortestCombination)):
                 shortestCombination = combination
+    memo[targetSum] = shortestCombination
     return shortestCombination
 
-print(bestSum(7, [5, 3, 4, 7]))
+print(bestSum(200, [25, 3, 4, 7]))
